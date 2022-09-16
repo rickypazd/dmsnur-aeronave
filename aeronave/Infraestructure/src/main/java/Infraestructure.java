9dc1f8dbbb1a8f031de824af6074ee6da7a3b830
rickypazd <rickypazd@hotmail.com>
2022-09-15 23:38:06 -0400

    as

--- aeronave/Infraestructure/src/main/java/Infraestructure.java
+++ aeronave/Infraestructure/src/main/java/Infraestructure.java
@@ -25,7 +25,9 @@ public class Infraestructure {
       config.AddConsumer(CheckInCreadoConsumer.class);
 
       config.UsingRabbitMq((context, cfg) -> {
-        cfg.Host = "192.168.3.2";
+        cfg.Host = "147.182.209.156";
+        cfg.User = "admin";
+        cfg.Password = "admin";
         // cfg.ReceiveEndpoint(CheckInCreadoConsumer.QueueName, endpoint -> {
         //   endpoint.ConfigureConsumer(CheckInCreadoConsumer.class);
         // });
--- aeronave/fourteam/src/main/java/fourteam/massTransit/RabbitMq.java
+++ aeronave/fourteam/src/main/java/fourteam/massTransit/RabbitMq.java
@@ -11,16 +11,24 @@ public class RabbitMq {
   private IMassTransit massTransit;
 
   public String Host;
+  public String User;
+  public String Password;
   private Connection connection;
   private Channel channel;
 
   public RabbitMq(IMassTransit massTransit) {
     this.massTransit = massTransit;
+    this.Host = "localhost";
+    this.User = "guest";
+    this.Password = "guest";
   }
 
   public void newConnection() throws IOException, TimeoutException {
     ConnectionFactory factory = new ConnectionFactory();
     factory.setHost(this.Host);
+    factory.setUsername(this.User);
+    factory.setPassword(this.Password);
+
     this.connection = factory.newConnection();
     this.channel = connection.createChannel();
     System.out.println("Conexion exitosa a rabbit");
